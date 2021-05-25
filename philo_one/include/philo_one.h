05ceac23bc0988f3862415fecd7bd6ecfc2af71c
Adrian Roque <adrian.w.roque@gmail.com>
2021-05-24 23:08:03 -0300

    Update header file

--- philo_one/include/philo_one.h
+++ philo_one/include/philo_one.h
@@ -3,16 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   philo_one.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
+/*   By: aroque <aroque@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 18:56:49 by aroque            #+#    #+#             */
-/*   Updated: 2021/05/23 18:57:27 by aroque           ###   ########.fr       */
+/*   Updated: 2021/05/24 22:56:54 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_ONE
-# define PHILO_ONE
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
 
+typedef enum e_errcode {
+	ERRSYS = 134,
+	EINVARG
+}	t_errcode;
+
+typedef s_params {
+	int philosophers;
+	int time_to_die;
+	int time_to_eat;
+	int time_to_die;
+	int eat_times;
+}	t_params;
+
+float	timestamp(struct timeval start);
+int		ft_atoi(const char *str)
 
 
 #endif
