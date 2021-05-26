c9515cadf334fa936decc1c17d35568b0bd3e329
Adrian Roque <adrian.w.roque@gmail.com>
2021-05-25 23:01:42 -0300

    Add parameter validation

---
+++ philo_one/src/args.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   args.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aroque <aroque@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/24 22:40:45 by aroque            #+#    #+#             */
+/*   Updated: 2021/05/25 22:54:54 by aroque           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_one.h"
+
+bool	validate_int(char *elem)
+{
+	if (!*elem)
+		return (true);
+	else if (!ft_isdigit(*elem))
+		return (false);
+	return (validate_int(elem + 1));
+}
+
+bool	validate_int_params(int argc, char **argv)
+{
+	if (!argc)
+		return (true);
+	if (!validate_int(argv[argc]))
+		return (false);
+	return (validate_int_params(argc - 1, argv));
+}
+
+t_errcode	get_params(int argc, char *argv[], t_params *params)
+{
+	if (argc < 5 || argc > 6)
+		return (EINVARG);
+	if (!validate_int_params(argc - 1, argv))
+		return (EINVARG);
+	params = malloc(sizeof(*params));
+	if (!params)
+		return (ERRSYS);
+	memset(params, 0, sizeof(*params));
+	params->philosophers = ft_atoi(argv[1]);
+	params->time_to_die = ft_atoi(argv[2]);
+	params->time_to_eat = ft_atoi(argv[3]);
+	params->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		params->eat_times = ft_atoi(argv[5]);
+	return (0);
+}
---
+++ philo_one/src/atoi.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atoi.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aroque <aroque@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/25 22:53:48 by aroque            #+#    #+#             */
+/*   Updated: 2021/05/25 22:57:25 by aroque           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_isspace(char str)
+{
+	if (str == 0x20 || (str >= 0x09 && str <= 0x0d))
+		return (1);
+	return (0);
+}
+
+static int	ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int	ft_atoi(const char *str)
+{
+	int	n;
+	int	aux;
+	int	signal;
+
+	n = 0;
+	aux = 0;
+	signal = -1;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '+' || *str == '-')
+		if (*str++ == '-')
+			signal = 1;
+	while (*str >= '0' && *str <= '9')
+	{
+		n = n * 10 - (*str++ - '0');
+		if (aux < n)
+		{
+			if (signal < 0)
+				return (-1);
+			return (0);
+		}
+		aux = n;
+	}
+	return (n * signal);
+}
