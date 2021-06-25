c5a684a5d1b22ec177847ca01038723dab3fdf91
Adrian Roque <adrian.w.roque@gmail.com>
2021-06-25 01:34:52 -0300

    Implement thread iterators

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: aroque <aroque@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 18:56:49 by aroque            #+#    #+#             */
-/*   Updated: 2021/06/20 21:14:56 by aroque           ###   ########.fr       */
+/*   Updated: 2021/06/25 01:28:11 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,26 +46,27 @@ typedef struct s_seat
 	int				t_sleep;
 	int				meals;
 	void			*(*routine)(void *);
-	void			*arg;
 }	t_seat;
 
 typedef struct	s_table
 {
 	unsigned int	n;
+	pthread_t		*phil;
 	pthread_mutex_t	*forks;
 	t_params		*params;
-
+	int	t_die;
+	int	t_eat;
+	int	t_sleep;
+	int	meals;
 }	t_table;
-
-//typedef struct s_thread_info
-//{
-//}	t_thread_info;
 
 float		timestamp(struct timeval start);
 int			ft_atoi(const char *str);
-t_errcode	get_params(int argc, char *argv[], t_params **params);
+t_errcode	get_params(int argc, char *argv[], t_table **params);
 int			pthread_mutex_init_wrapper(void *mutex);
 int			pthread_create_wrapper(void *thread_info);
 int			pthread_join_wrapper(void *thread);
+int			iterate_void(int (*func)(void *), void *arr, int n);
+int			iterate(int (*func)(void *), t_seat *arr, int n);
 
 #endif
--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,101 +6,76 @@
 /*   By: aroque <aroque@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 18:58:26 by aroque            #+#    #+#             */
-/*   Updated: 2021/06/20 20:56:16 by aroque           ###   ########.fr       */
+/*   Updated: 2021/06/25 01:30:44 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <pthread.h>
+#include <string.h>
 
 void *philosopher(void *arg)
 {
 	t_seat	*seat;
 
 	seat = arg;
-	printf("philo %d is thinking.", seat->i);
+	printf("philo %d is thinking.\n", seat->i);
 	usleep(1000);
 	return (0);
 }
 
-t_seat	*prepare_seat(unsigned int i, pthread_mutex_t *forks, t_params *p)
+t_seat	prepare_seat(unsigned int i, t_table *table)
 {
-	t_seat	*seat;
+	t_seat		seat;
 	
-	seat = malloc(sizeof(*seat));
-	if (!seat)
-		return (NULL);
-	memset(seat, 0, sizeof(*seat));
-	seat->i = i;
-	seat->left_fork = &forks[i];
-	seat->left_fork = &forks[(i + 1) % p->n];
-	seat->t_die = p->t_die;
-	seat->t_eat = p->t_eat;
-	seat->t_sleep = p->t_sleep;
+	seat.i = i;
+	seat.phil = &table->phil[i];
+	seat.left_fork = &table->forks[i];
+	seat.right_fork = &table->forks[(i + 1) % table->n];
+	seat.t_die = table->t_die;
+	seat.t_eat = table->t_eat;
+	seat.t_sleep = table->t_sleep;
+	seat.meals = table->meals;
+	seat.routine = philosopher;
 	return (seat);
 }
 
 
-t_seat **init_thread_info(unsigned int n, pthread_mutex_t *forks, t_params *params)
+int	start_seats(t_table *table, t_seat seats[])
 {
 	unsigned int	i;
-	//t_seat			*seat;
-	t_seat			**arr;
 
-	i = 0;
-	arr = malloc(n * sizeof(*arr));
-	if (!arr)
-		return (NULL);
-	while (i < n)
-	{
-		arr[i] = prepare_seat(i, forks, params);
-		i++;
-	}
-	return (arr);
-
+	i = -1;
+	while (++i < table->n)
+		seats[i] = prepare_seat(i, table);
+	return (0);
 }
 
-int	start_the_party(t_params *params)
+int	start_the_party(t_table *table)
 {
-	int				err;
-	unsigned int	n;
-	unsigned int	i;
-	t_seat			*phil;
-	pthread_mutex_t	*forks;
+	pthread_t		phil[table->n];
+	pthread_mutex_t	forks[table->n];
+	t_seat			seats[table->n];
 
-	i = 0;
-	err = 0;
-	n = params->n;
-	forks = malloc(n * sizeof(*forks));
-	phil = malloc(n * sizeof(*phil));
-	iterate(pthread_mutex_init_wrapper, forks, n);
-	//phil = initialize_thread_info(philosopher, seat);
-	//iterate(pthread_create_wrapper, phil, params->n);
-	//iterate(prepare_seat, phil, n);
-	while (!err && i < params->n)
-	{
-		seat = prepare_seat(i, forks, params);
-		err = pthread_create(&phil[i], NULL, philosopher, seat);
-		i++;
-	}
-	//while (!err && i < params->n)
-	//{
-	//	err = pthread_join(phil[i], NULL);
-	//	i++;
-	//}
-	return (err);
+	table->forks = forks;
+	table->phil = phil;
+	start_seats(table, seats);
+	iterate_void(pthread_mutex_init_wrapper, table->forks, table->n);
+	iterate(pthread_create_wrapper, seats, table->n);
+	iterate(pthread_join_wrapper, seats, table->n);
+	return (0);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_errcode	err;
-	t_params	*params;
+	t_table		*table;
 
-	params = NULL;
-	err = get_params(argc, argv, &params);
+	table = NULL;
+	err = get_params(argc, argv, &table);
 	if (err)
 		printf("Error\n");
-    else
-		err = start_the_party(params);
+	else
+		err = start_the_party(table);
 	return (err);
 }
